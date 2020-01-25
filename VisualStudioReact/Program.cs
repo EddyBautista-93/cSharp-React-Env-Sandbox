1c0fb0a0d08278707094e855762869ac8fcf9991
Eddy Bautista <bautista.j.eddy@gmail.com>
2020-01-25 13:09:53 -0600

    First Commit

--- VisualStudioReact/Program.cs
+++ VisualStudioReact/Program.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Linq;  
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
