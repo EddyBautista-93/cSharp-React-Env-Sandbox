984a880496deb318f73b87553870388c00890367
Eddy Bautista <bautista.j.eddy@gmail.com>
2020-02-09 12:37:54 -0600

    * NavMenu.js: FInally fixed the issue with npm so back to the   project at hand!!!
    * VisualStudioReact.csproj: Added name instead of prjoject name
    * HelloWorldController.cs: - test case controller understanding mvc in dotnet

---
+++ VisualStudioReact/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace VisualStudioReact.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my deault action";
+        }
+        // grabs the key of name and the value set anf the numTImes to the value
+        //at the moment it is put in the url for the demo purpose of this exercise 
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+    }
+}
